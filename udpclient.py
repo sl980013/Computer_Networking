3051c6614dc51077339e4e80b055d5b6cfcc9aac
Seoeun Lee <72355870+sl980013@users.noreply.github.com>
2022-05-02 05:49:47 +0100

    Updated udp server and client-profanity filter, variable, server socket close

--- udpclient.py
+++ udpclient.py
@@ -4,43 +4,49 @@ import json
 import rsa
 import datetime
 import zlib
+from better_profanity import profanity
 
+""""
+Use CRC32 to calculate a checksum for our data to be sent
+Take json data to compare the checksum with the calculated checksum in json object
+checksum is applied on the type and message content of the json
+"""""
 
-# Uses CRC32 to calculate a checksum for our data to be sent
-# takes in json data and compares the checksum in the json object with the calculated checksum
-# checksum is performed on the type and content of the json
-def checksum_calculator(data):
+def calculate_checksum(data):
     name = data.get("type")
-    content = data.get("content")
+    message_content = data.get("content")
     if name is None:
         name = ""
-    if content is None:
-        content = ""
-    checksum_sequence = name + content
+    if message_content is None:
+        message_content = ""
+    checksum_seq = name + message_content
 
-    checksum = zlib.crc32(bytes(checksum_sequence, "utf-8"))
+    checksum = zlib.crc32(bytes(checksum_seq, "utf-8"))
     return checksum
 
 
-def send_data(sock, jsonData):
+def send_data(sock, json_data):
 
-    # calculates checksum and puts it in json object
-    checksumVal = checksum_calculator(jsonData)
+    # checksum calculation- puts it in json object
+    checksum_value = calculate_checksum(json_data)
     # append checksum to json
-    jsonData['checksum'] = checksumVal
+    json_data['checksum'] = checksum_value
 
-    jsonData = json.dumps(jsonData)
+    json_data = json.dumps(json_data)
 
     # Attempts to send the data to the recipient
-    sock.sendto(jsonData.encode(), UDP_ADDRESS)
+    sock.sendto(json_data.encode(), UDP_ADDRESS)
 
     try:
-    # After every message is sent there should be an ACK packet sent back to confirm it's arrival,
-    # Else it will attempt to resend the data one more time before moving on to the next address
+        """
+        After every message is sent there should be an ACK packet sent back to confirm it's arrival,
+        Else it will attempt to resend the data one more time before moving on to the next address
+        """
+
         data, server = sock.recvfrom(4096)
-        jsonObj = json.loads(data)
-        jpacket = jsonObj.get("type")
-    # if an acknowledgement message is received then we know the data reached the recipient correctly
+        jobject = json.loads(data)
+        jpacket = jobject.get("type")
+    # if an acknowledgement message is received, we know the data reached the recipient
         if jpacket == "ack":
             print("ACK packet received")
         else:
@@ -56,18 +62,18 @@ def receive_data(sock, expected_type):
     # Attempts to receive data from recipient
     data, server = sock.recvfrom(4096)
 
-    jsonObj = json.loads(data)
+    jobject = json.loads(data)
 
     # Checks if the received packet has a checksum as it was optional for this implementation
     # if the checksums do not match then it will raise an exception
-    if jsonObj.get("checksum") is not None:
-        if checksum_calculator(jsonObj) != int(jsonObj.get("checksum")):
+    if jobject.get("checksum") is not None:
+        if calculate_checksum(jobject) != int(jobject.get("checksum")):
             print("Checksums do not match, need the packet resent")
             raise Exception()
         else:
             print("Checksums match")
 
-    jpacket = jsonObj.get("type")
+    jpacket = jobject.get("type")
 
     print("Packet Type Received: ", jpacket)
 
@@ -76,62 +82,53 @@ def receive_data(sock, expected_type):
         raise Exception()
 
     if jpacket == "recipient_public_key":
-        global recipientKey
-        recipientKey =  rsa.PublicKey.load_pkcs1(jsonObj.get("content").encode())
+        global receiver_key
+        receiver_key = rsa.PublicKey.load_pkcs1(jobject.get("content").encode())
     elif jpacket == "recipient_username":
-        global recipientUsername
-        recipientUsername = rsa.decrypt(jsonObj.get("content").encode('latin-1'), privateKey).decode()
+        global receiver_name
+        receiver_name = rsa.decrypt(jobject.get("content").encode('latin-1'), privateKey).decode()
 
     elif jpacket == "message":
-        print(rsa.decrypt(jsonObj.get("content").encode('latin-1'), privateKey).decode())
-
+        print(profanity.censor(rsa.decrypt(jobject.get("content").encode('latin-1'), privateKey).decode()))
     # sends an ACK packet back to confirm the data was received
     data = {"type": "ack"}
-    jsonData = json.dumps(data)
-    sock.sendto(jsonData.encode(), UDP_ADDRESS)
-
-
+    json_data = json.dumps(data)
+    sock.sendto(json_data.encode(), UDP_ADDRESS)
 
 local_user = os.getlogin()
 
-## Generates a new RSA private and public key
+# Generates a new RSA private and public key for asymmetric encryption
 publicKey, privateKey = rsa.newkeys(2048)
 
 
-# Defining global variables
-global recipientKey
-global recipientUsername
-
-
-
-
+# Define global variables
+global receiver_key
+global receiver_name
 
 receiver_list = ""
 while receiver_list == "":
-    receiver_list = str(input("Enter your list of IP addresses you want to send greetings to with commas separating the addresses (e.g '127.0.0.1, 127.0.0.2, 127.0.0.3' ):\n"))
+    receiver_list = str(input("Enter the list of IP addresses you want to send greetings to with commas separating the addresses (e.g '127.0.0.1, 127.0.0.2, 127.0.0.3' ):\n"))
 
 receiver_list = receiver_list.replace(" ", "")
 receiver_list = receiver_list.split(",")
 print(receiver_list)
 
-# This custom Message is optional and will be taken at the start of program execution
-customMessage = str(input("Please enter your custom message (optional): "))
+# This message is optional. It will be taken at the start of program execution
+optional_message = str(input("Please enter a message (optional): "))
 
-
-
-
-# Attempts to send the greeting to each IP address that the user entered
+# Attempts to send the greeting to each IP address that the user has entered
 for i, receiver in enumerate(receiver_list):
 
-###### Creating socket for current IP
-
+    """
+    Creating socket for current IP
+    """
     # Change the IP Address to the list of IPs
     UDP_IP_ADDRESS = receiver
     try:
         socket.inet_aton(UDP_IP_ADDRESS)
-        # IP is legal if passes
+        # IP is legal if it's passed
     except socket.error:
-        # Not legal
+        # If it's not legal
         print("IP is not valid")
         # continue, used to end the for loop iteration for the IP entered
         continue
@@ -140,41 +137,42 @@ for i, receiver in enumerate(receiver_list):
     UDP_PORT_NO = 12001
     UDP_ADDRESS = (UDP_IP_ADDRESS, UDP_PORT_NO)
     socket.setdefaulttimeout(1)
-    clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-
-
-###### Synchronize packet for initiating connection
+    """
+    Synchronize packet for initiating connection
+    """
     print("\n\nInitialising connection")
     data = {"type": "sync"}
 
     # Tries to send the json payload to the address
     try:
-        send_data(clientSock, data)
+        send_data(client_socket, data)
     except socket.timeout as inst:
-        # If the request times out it attempts to resend the data once then will move on to the next address if that fails
+        # If the request times out, it attempts resending the data once. It moves on to the next address if that fails
         print("Request timed out - Resending Data")
         try:
-            send_data(clientSock, data)
+            send_data(client_socket, data)
         except:
             print("Error, cannot establish connection with current IP, moving on to next address\n\n")
-            clientSock.close()
+            client_socket.close()
             continue
     except socket.error:
         print("Error with connection - Resending Data")
         try:
-            send_data(clientSock, data)
+            send_data(client_socket, data)
         except:
             print("Error, cannot establish connection with current IP, moving on to next address\n\n")
-            clientSock.close()
+            client_socket.close()
             continue
     except:
         print("Unknown error occurred, moving on to next IP address\n\n")
-        clientSock.close()
+        client_socket.close()
         continue
 
-
-###### Exchange Public keys between receiver and sender
+    """
+    Exchange Public keys between receiver and sender
+    """
     print("Exchanging public keys")
     # Converts public key to PEM format as bytes then decodes it to string
     publicKey_packet = publicKey.save_pkcs1().decode()
@@ -182,197 +180,197 @@ for i, receiver in enumerate(receiver_list):
 
     # Tries to send the json payload to the address
     try:
-        send_data(clientSock, data)
+        send_data(client_socket, data)
     except socket.timeout as inst:
-        # If the request times out it attempts to resend the data once then will move on to the next address if that fails
+        # If the request times out, it attempts resending the data once. It then moves on to the next address if that fails
         print("Request timed out - Resending Data")
         try:
-            send_data(clientSock, data)
+            send_data(client_socket, data)
         except:
             print("Error, cannot establish connection with current IP, moving on to next address\n\n")
-            clientSock.close()
+            client_socket.close()
             continue
     except socket.error:
         print("Error with connection - Resending Data")
         try:
-            send_data(clientSock, data)
+            send_data(client_socket, data)
         except:
             print("Error, cannot establish connection with current IP, moving on to next address\n\n")
-            clientSock.close()
+            client_socket.close()
             continue
     except:
         print("Unknown error occurred, moving on to next IP address\n\n")
-        clientSock.close()
+        client_socket.close()
         continue
 
 
     try:
-        receive_data(clientSock, "recipient_public_key")
+        receive_data(client_socket, "recipient_public_key")
     except socket.timeout as inst:
         print("Socket timed out, trying again")
         try:
-            receive_data(clientSock, "recipient_public_key")
+            receive_data(client_socket, "recipient_public_key")
         except:
             print("An Error occurred receiving data from the current IP, moving on to next address\n\n")
-            clientSock.close()
+            client_socket.close()
             continue
     except:
         print("An error occurred, trying again")
         try:
-            receive_data(clientSock, "recipient_public_key")
+            receive_data(client_socket, "recipient_public_key")
         except:
             print("An Error occurred receiving data from the current IP, moving on to next address\n\n")
-            clientSock.close()
+            client_socket.close()
             continue
 
-
-
-
-###### Sending a request for the recipient username
+    """
+    Sends a request for the recipient username
+    """
     print("Asking for recipient username")
     data = {"type": "request_username"}
     # Tries to send the json payload to the address
     try:
-        send_data(clientSock, data)
+        send_data(client_socket, data)
     except socket.timeout as inst:
         # If the request times out it attempts to resend the data once then will move on to the next address if that fails
         print("Request timed out - Resending Data")
         try:
-            send_data(clientSock, data)
+            send_data(client_socket, data)
         except:
             print("Error, cannot establish connection with current IP, moving on to next address\n\n")
-            clientSock.close()
+            client_socket.close()
             continue
     except socket.error:
         print("Error with connection - Resending Data")
         try:
-            send_data(clientSock, data)
+            send_data(client_socket, data)
         except:
             print("Error, cannot establish connection with current IP, moving on to next address\n\n")
-            clientSock.close()
+            client_socket.close()
             continue
     except:
         print("Unknown error occurred, moving on to next IP address\n\n")
-        clientSock.close()
+        client_socket.close()
         continue
 
-
-#### Receiving recipient username
+    """
+    Receiving recipient username
+    """
     try:
-        receive_data(clientSock, "recipient_username")
+        receive_data(client_socket, "recipient_username")
     except socket.timeout as inst:
         print("Socket timed out, trying again")
         try:
-            receive_data(clientSock, "recipient_username")
+            receive_data(client_socket, "recipient_username")
         except:
             print("An Error occurred receiving data from the current IP, moving on to next address\n\n")
-            clientSock.close()
+            client_socket.close()
             continue
     except:
         print("An error occurred, trying again")
         try:
-            receive_data(clientSock, "recipient_username")
+            receive_data(client_socket, "recipient_username")
         except:
             print("An Error occurred receiving data from the current IP, moving on to next address\n\n")
-            clientSock.close()
+            client_socket.close()
             continue
+    print(receiver_name)
 
-    print(recipientUsername)
-
-###### Sending the greeting message
-
+    """ 
+    Sending the greeting message
+    """
     # Checks time to put in correct greeting of Good Morning, Good Afternoon or Good Evening
     currentTime = datetime.datetime.now().hour
 
     if currentTime < 12:
-        greeting = "Good Morning "
+        greeting = "Good Morning, "
     elif 12 <= currentTime < 18:
-        greeting = "Good Afternoon "
+        greeting = "Good Afternoon, "
     else:
-        greeting = "Good Evening "
+        greeting = "Good Evening, "
 
-    message = "\n-----\n"+greeting + recipientUsername+ ".\n"+customMessage + "\n\nFrom: " +local_user+"\n-----\n"
-
-
+    message = "\n-----\n"+greeting + receiver_name + ".\n"+optional_message + "\n\nFrom: " + local_user +"\n-----\n"
     print("Sending Message: \n",message+"\n")
-    message = rsa.encrypt(message.encode(), recipientKey).decode('latin-1')
+    message = rsa.encrypt(message.encode(), receiver_key).decode('latin-1')
 
     data = {"type": "message", "content":message}
 
     # Tries to send the json payload to the address
     try:
-        send_data(clientSock, data)
+        send_data(client_socket, data)
     except socket.timeout as inst:
-        # If the request times out it attempts to resend the data once then will move on to the next address if that fails
+        # If the request times out it attempts to resend the data. It moves on to the next address if that fails
         print("Request timed out - Resending Data")
         try:
-            send_data(clientSock, data)
+            send_data(client_socket, data)
         except:
             print("Error, cannot establish connection with current IP, moving on to next address\n\n")
-            clientSock.close()
+            client_socket.close()
             continue
     except socket.error:
         print("Error with connection - Resending Data")
         try:
-            send_data(clientSock, data)
+            send_data(client_socket, data)
         except:
             print("Error, cannot establish connection with current IP, moving on to next address\n\n")
-            clientSock.close()
+            client_socket.close()
             continue
     except:
         print("Unknown error occurred, moving on to next IP address\n\n")
-        clientSock.close()
+        client_socket.close()
         continue
 
-#### Receiving a response message
+    """
+    Receiving a response message
+    """
     try:
-        receive_data(clientSock, "message")
+        receive_data(client_socket, "message")
     except socket.timeout as inst:
         print("Socket timed out, trying again")
         try:
-            receive_data(clientSock, "message")
+            receive_data(client_socket, "message")
         except:
             print("An Error occurred receiving data from the current IP, moving on to next address\n\n")
-            clientSock.close()
+            client_socket.close()
             continue
     except:
         print("An error occurred, trying again")
         try:
-            receive_data(clientSock, "message")
+            receive_data(client_socket, "message")
         except:
             print("An Error occurred receiving data from the current IP, moving on to next address\n\n")
-            clientSock.close()
+            client_socket.close()
             continue
 
-##### Ending the connection with the current receipient as the message has been sent successfully
+    """
+    Ending the connection with the current receipient as the message has been sent successfully
+    """
     data = {"type": "fin"}
     # Tries to send the json payload to the address
     try:
-        send_data(clientSock, data)
+        send_data(client_socket, data)
     except socket.timeout as inst:
         # If the request times out it attempts to resend the data once then will move on to the next address if that fails
         print("Request timed out - Resending Data")
         try:
-            send_data(clientSock, data)
+            send_data(client_socket, data)
         except:
             print("Error, cannot establish connection with current IP, moving on to next address\n\n")
-            clientSock.close()
+            client_socket.close()
             continue
     except socket.error:
         print("Error with connection - Resending Data")
         try:
-            send_data(clientSock, data)
+            send_data(client_socket, data)
         except:
             print("Error, cannot establish connection with current IP, moving on to next address\n\n")
-            clientSock.close()
+            client_socket.close()
             continue
     except:
         print("Unknown error occurred, moving on to next IP address\n\n")
-        clientSock.close()
+        client_socket.close()
         continue
 
-
-
-    clientSock.close()
+    client_socket.close()
 
     print("Terminated connection with recipient - " + str(UDP_IP_ADDRESS))
--- udpserver.py
+++ udpserver.py
@@ -2,18 +2,19 @@ import socket
 import os
 import json
 import rsa
+from better_profanity import profanity
 
 
-def send_data(sock, jsonData):
+def send_data(sock, json_data):
     # Attempts to send the data to the recipient
     global client
-    sock.sendto(jsonData.encode(), client)
+    sock.sendto(json_data.encode(), client)
     # After every message is sent there should be an ACK packet sent back to confirm it's arrival,
     # Else it will attempt to resend the data one more time before moving on to the next address
     try:
         data, server = sock.recvfrom(4096)
-        jsonObj = json.loads(data)
-        jpacket = jsonObj.get("type")
+        jobject = json.loads(data)
+        jpacket = jobject.get("type")
         # if an acknowledgement message is received then we know the data reached the recipient correctly
         if (jpacket == "ack"):
             print("ACK packet received")
@@ -24,7 +25,7 @@ def send_data(sock, jsonData):
         print("socket timed out")
 
 
-def receive_data(sock, expectedMessageType):
+def receive_data(sock, expected_message_type):
     # Attempts to receive data from recipient
     data, address = sock.recvfrom(4096)
 
@@ -32,114 +33,130 @@ def receive_data(sock, expectedMessageType):
     global client
     client = address
 
-    jsonObj = json.loads(data)
-    jpacket = jsonObj.get("type")
+    jobject = json.loads(data)
+    jpacket = jobject.get("type")
 
-    if jpacket != expectedMessageType:
+    if jpacket != expected_message_type:
         raise Exception()
 
     if jpacket == "sync":
-        print("Connection initialized with: ",client)
+        print("Connection initialized with: ", client)
 
     elif jpacket == "sender_public_key":
         global senderKey
-        senderKey = rsa.PublicKey.load_pkcs1(jsonObj.get("content").encode())
+        senderKey = rsa.PublicKey.load_pkcs1(jobject.get("content").encode())
 
     elif jpacket == "message":
-        print(rsa.decrypt(jsonObj.get("content").encode('latin-1'), privateKey).decode() )
+        print(profanity.censor(rsa.decrypt(jobject.get("content").encode('latin-1'), privateKey).decode()))
 
     elif jpacket == "fin":
         print("Terminating connection")
 
     # sends an ACK packet back to confirm the data was received
     data = {"type": "ack"}
-    jsonData = json.dumps(data)
-    sock.sendto(jsonData.encode(), address)
+    json_data = json.dumps(data)
+    sock.sendto(json_data.encode(), address)
 
 local_user = os.getlogin()
 
 global senderKey
 
-## Generates a new RSA private and public key
+""" 
+Generates a new RSA private and public key
+"""
 publicKey, privateKey = rsa.newkeys(2048)
 
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-serverSocket.bind(('127.0.0.1', 12001))
+server_socket.bind(('127.0.0.1', 12001))
 
-serverSocket.settimeout(2)
+server_socket.settimeout(2)
 
 
 # Client that's sending messages to it currently
 global client
-
-
 
 print("Started receiver server")
 
 while True:
     # client set to null
     client = ()
-##### A connection is established, the client is set
 
+    """
+    A connection is established, the client is set
+    """
     try:
-        receive_data(serverSocket, "sync")
+        receive_data(server_socket, "sync")
     except:
         continue
 
-#### Encryption public key is received
+    """ 
+    Encryption public key is received
+    """
     try:
-        receive_data(serverSocket, "sender_public_key")
+        receive_data(server_socket, "sender_public_key")
     except:
         continue
 
-### Sending our own encryption public key
+    """
+    Sending our own encryption public key
+    """
     # Converts public key to PEM format as bytes
     publicKey_packet = publicKey.save_pkcs1().decode()
-    message = {"type": "recipient_public_key", "content": publicKey_packet }
+    message = {"type": "recipient_public_key", "content": publicKey_packet}
     data = json.dumps(message)
     try:
-        send_data(serverSocket, data)
+        send_data(server_socket, data)
     except:
         continue
 
-#### Our username is requested
+    """
+    Our username is requested
+    """
     try:
-        receive_data(serverSocket, "request_username")
+        receive_data(server_socket, "request_username")
     except socket.timeout as inst:
         print("Exception occurred at 4")
         continue
 
-#### Sending our username
-
+    """
+    Sending our username
+    """
     # This RSA algorithm encrypts with latin-1 encoding
     encryptedUsername = rsa.encrypt(local_user.encode(), senderKey).decode('latin-1')
 
     data = {"type": "recipient_username", "content": encryptedUsername}
     data = json.dumps(data)
     try:
-        send_data(serverSocket, data)
+        send_data(server_socket, data)
     except:
         continue
 
-#### A message is received
+    """
+    A message is received
+    """
     try:
-        receive_data(serverSocket, "message")
+        receive_data(server_socket, "message")
     except:
         continue
 
-#### Replying with our own message
+    """
+    Reply with our own message
+    """
     message = "\n\nThank you for your message!\n\n"
     message = rsa.encrypt(message.encode(), senderKey).decode('latin-1')
     data = {"type": "message", "content": message}
     data = json.dumps(data)
     try:
-        send_data(serverSocket, data)
+        send_data(server_socket, data)
     except:
         continue
 
-#### Ending communication with client
+    """
+    Ending communication with client
+    """
     try:
-        receive_data(serverSocket, "fin")
+        receive_data(server_socket, "fin")
+        server_socket.close()
     except:
         print("closed")
